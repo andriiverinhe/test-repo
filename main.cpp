3ac0c9cdf32826f73ebf108d92b36a579ab53b9e
Andrew <an.veringe@gmail.com>
2024-12-09 10:58:50 +0500

    добавлены тесты из пункта 2.2, для метода contains

--- main.cpp
+++ main.cpp
@@ -7,47 +7,64 @@
 
 /*< Tests for the `body_contains` method >*/
 bool test_body_contains_red_candle() {
-  Candle redCandle(12, 15, 5, 10);
-  bool result = true;
+  Candle c(12, 15, 5, 10);
+  bool res = true;
 
-  if (redCandle.body_contains(11) == false) result = false;
-  if (redCandle.body_contains(12) == false) result = false;
-  if (redCandle.body_contains(10) == false) result = false;
-  if (redCandle.body_contains(9) == true) result = false;
-  if (redCandle.body_contains(13) == true) result = false;
+  if (c.body_contains(11) == false) res = false;
+  if (c.body_contains(12) == false) res = false;
+  if (c.body_contains(10) == false) res = false;
+  if (c.body_contains(9) == true) res = false;
+  if (c.body_contains(13) == true) res = false;
 
-  return result;
+  return res;
 }
 
 bool test_body_contains_green_candle() {
-  Candle greenCandle(10, 15, 5, 12);
-  bool result = true;
+  Candle c(10, 15, 5, 12);
+  bool res = true;
 
-  if (greenCandle.body_contains(11) == false) result = false;
-  if (greenCandle.body_contains(10) == false) result = false;
-  if (greenCandle.body_contains(12) == false) result = false;
-  if (greenCandle.body_contains(9) == true) result = false;
-  if (greenCandle.body_contains(13) == true) result = false;
+  if (c.body_contains(11) == false) res = false;
+  if (c.body_contains(10) == false) res = false;
+  if (c.body_contains(12) == false) res = false;
+  if (c.body_contains(9) == true) res = false;
+  if (c.body_contains(13) == true) res = false;
 
-  return result;
+  return res;
 }
 
 bool test_body_contains_doji_candle() {
-  Candle neutralCandle(10, 10, 5, 10);
-  bool result = true;
+  Candle c(10, 10, 5, 10);
+  bool res = true;
 
-  if (neutralCandle.body_contains(10) == false) result = false;
-  if (neutralCandle.body_contains(9) == true) result = false;
-  if (neutralCandle.body_contains(11) == true) result = false;
+  if (c.body_contains(10) == false) res = false;
+  if (c.body_contains(9) == true) res = false;
+  if (c.body_contains(11) == true) res = false;
 
-  return result;
+  return res;
 }
 
-/*< >*/
+/*< Tests for the `contains` method >*/
+bool test_contains_within_range() {
+  Candle c(10, 15, 5, 12);
+  return c.contains(7); 
+}
+
+bool test_contains_below_range() {
+  Candle c(10, 15, 5, 12);
+  return (c.contains(1) == false);  
+}
+
+bool test_contains_above_range() {
+  Candle c(10, 15, 5, 12);
+  return (c.contains(20) == false); 
+}
+
+/*<  >*/
 
 int main() {
   TestCandel tc;
   tc.initTests({test_body_contains_red_candle, test_body_contains_green_candle,
-                test_body_contains_doji_candle});
+                test_body_contains_doji_candle, test_contains_within_range,
+                test_contains_below_range, test_contains_above_range});
   return tc.launchTests();
 }
