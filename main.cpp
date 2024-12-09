880edf702e098fb97a7a1e03d25c7b60d96f399d
Andrew <an.veringe@gmail.com>
2024-12-09 11:18:53 +0500

    добавлены тесты из пункта 2.5, для метода is_red

--- main.cpp
+++ main.cpp
@@ -89,7 +89,23 @@ bool test_body_size_3() {
   Candle c(55, 2, 18, -45);
   return (c.body_size() == 100);
 }
-/*<  >*/
+/*< Tests for the `is_red` method >*/
+bool test_is_red_1() {
+  Candle c(10, 5, 15, 12);
+  return (c.is_red() == false);
+}
+
+bool test_is_red_2() {
+  Candle c(12, 5, 15, 10);
+  return (c.is_red() == true);
+}
+
+bool test_is_red_3() {
+  Candle c(12, 5, 15, 12);
+  return (c.is_red() == false);
+}
+/*< Tests for the `is_green` method >*/
+
 
 int main() {
   TestCandel tc;
@@ -97,6 +113,7 @@ int main() {
                 test_body_contains_doji_candle, test_contains_within_range,
                 test_contains_below_range, test_contains_above_range,
                 test_full_size_1, test_full_size_2, test_full_size_3,
-                test_body_size_1, test_body_size_2, test_body_size_3});
+                test_body_size_1, test_body_size_2, test_body_size_3,
+                test_is_red_1, test_is_red_2, test_is_red_3});
   return tc.launchTests();
 }
